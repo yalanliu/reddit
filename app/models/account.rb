4251a19dd448655a5be46799a5df884f0dee5b40
yalan <lanya4190@gmail.com>
2020-03-16 22:11:40 +0800

    add account profile view

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,4 +4,10 @@ class Account < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+
+  validates_presence_of :first_name, :last_name, :username
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
